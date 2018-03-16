3236a00c35ef6cbd48074a7480ab48e6eeeae315
Mati Leon <mperalta@hexata.com>
2018-03-16 00:37:21 -0300

    Fix fibonacci

--- Fibonacci Number/Fibonacci Number.cs
+++ Fibonacci Number/Fibonacci Number.cs
@@ -11,16 +11,16 @@ namespace Fibonacci_Number
         static void Main(string[] args)
         {
             var numberString = Console.ReadLine();
-            var number = int.Parse(numberString) - 1;
+            var number = int.Parse(numberString) + 2;
 
-            var array = new Int64[int.Parse(numberString)];
+            var array = new Int64[number];
             array[0] = 0;
             array[1] = 1;
-            for (int i = 2; i < number; i++)
+            for (int i = 2; i <= int.Parse(numberString) ; i++)
             {
                 array[i] = array[i-1] + array[i-2];
             }
-            Console.WriteLine(array[number - 1]);
+            Console.WriteLine(array[int.Parse(numberString)]);
         }
     }
 }
