0b508855460ba8112a252aa1dfec6dd1c22e5cd4
Mati Leon <matias.peralta@set.ypf.com>
2018-03-22 18:23:28 -0300

    MmoneyChange

---
+++ LastDigitOfSumFibo/LastDigitOfSumFibo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LastDigitOfSumFibo
+{
+    class LastDigitOfSumFibo
+    {
+        static void Main(string[] args)
+        {
+            var numberString = Console.ReadLine();
+            var inputNumber = int.Parse(numberString);
+            var number = inputNumber + 2;
+
+            var array = new Int64[number];
+            array[0] = 0;
+            array[1] = 1;
+            for (int i = 2; i <= int.Parse(numberString); i++)
+            {
+                array[i] = array[i - 1] + array[i - 2];
+            }
+            Console.WriteLine(array[inputNumber]);
+        }
+    }
+}
---
+++ MoneyChange/MoneyChange.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+
+namespace MoneyChange
+{
+    class MoneyChange
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var number = int.Parse(input);
+
+            var diez = GetMoney(number, 10);
+            var cinco = GetMoney(number - diez * 10, 5);
+            var uno = GetMoney(number - cinco *5, 1);
+            Console.WriteLine(diez + cinco + uno);
+            Console.ReadLine();
+        }
+
+        private static int GetMoney(int number, int moneyChange)
+        {
+            var moneyChangeCount = 0;
+            var dif = number - moneyChange;
+            
+            while (dif > moneyChange)
+            {
+                
+                dif = dif - moneyChange;
+                moneyChangeCount++;
+            }
+
+            return moneyChangeCount;
+        }
+    }
+}
